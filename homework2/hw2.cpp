6b731a88c2857bd1b4eacadfc5f14d91cf6037e1
katushapvlva <93945568+katushapvlva@users.noreply.github.com>
2021-11-08 23:22:13 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -3,3 +3,20 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+
+using namespace std;
+
+double get_t(double x0, double x, double vx,int dir)
+{
+    return (x - x0)/(dir*vx);
+}
+
+double get_vy(double vy0, double t)
+{
+    return vy0 - 9.81*t;
+}
+
+double get_height(double h0, double vy, double t)
+{
+    return h0 + vy*t - 9.81/2 * pow(t, 2);
+}
