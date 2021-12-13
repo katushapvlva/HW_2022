d0c74b9059a80fdf9487f07d4e3d6f1e39b3dcd0
katushapvlva <93945568+katushapvlva@users.noreply.github.com>
2021-12-13 21:09:33 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -9,9 +9,8 @@ int main()
 
 	int x, y;
 	
-	int lx = 0, rx = 0, ly = 0, ry = 0;
-	double _rcos, _lcos;
-	_lcos = 1; _rcos = 1;
+	int x_left = 0, x_right = 0, y_left = 0, y_right = 0;
+	double cos_right = 1, cos_left = 1;
 
 	int x0, y0;
 	infile >> x0 >> y0;
@@ -22,19 +21,19 @@ int main()
 		double cos = (x0*x+y0*y)/(sqrt(x*x+y*y)*sqrt(x0*x0+y0*y0));
 		cos = std::round(cos * 10000000000.0) / 10000000000.0;
         
-		if (cos <= _lcos && (y0*x < x0*y)) 
+		if (cos <= cos_left && (y0*x < x0*y)) 
 		{
-			_lcos = cos;
-			lx = x; ly = y;
+			cos_left = cos;
+			x_left = x; y_left = y;
 
 		}
-		if (cos <= _rcos && (y0*x >= x0*y)) 
+		if (cos <= cos_right && (y0*x >= x0*y)) 
 		{
-			_rcos = cos;
-			rx = x; ry = y;
+			cos_right = cos;
+			x_right = x; y_right = y;
 		}
 	}
 
-	std::cout << "Leftmost: " << lx << " " << ly << "\n";
-	std::cout << "Rightmost: " << rx << " " << ry << "\n";
+	std::cout << "Leftmost: " << x_left << " " << y_left << "\n";
+	std::cout << "Rightmost: " << x_right << " " << y_right << "\n";
 }
